4b1465af88fc42ece6fb2416c64760453891343a
Rajni <rajnikant12345@gmail.com>
2019-03-04 09:58:26 +0530

    Update main.go

--- GenerateString/main.go
+++ GenerateString/main.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+	"fmt"
+)
+
+func KString(A []int, n int, k int) {
+	if n < 1 {
+		fmt.Println(A)
+		return
+	}
+	for i := 0;i<k;i++ {
+		A[n-1] = i 
+		KString(A,n-1,k)
+	}
+}
+
+func main() {
+	l := 5
+	A := make([]int,l)
+	KString(A,l,l)
+}
