13286e64a8dc6890a612239fd46523cc0f476b31
Rajni <rajnikant12345@gmail.com>
2019-03-04 09:29:00 +0530

    Update main.go

--- GenerateBinayNum/main.go
+++ GenerateBinayNum/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+)
+
+func Binary(A []int, n int) {
+	if n == len(A) {
+		fmt.Println(A);return
+	}
+	A[n] = 0 ; Binary(A,n+1) ; A[n] = 1 ; Binary(A,n+1)
+}
+
+func main() {
+	A := make([]int,8)
+	Binary(A,0)
+}
